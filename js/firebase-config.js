6082cf3777070e03ca66437ac22a5943b8026b01
bielfg12 <gabriel.fernandesagil@gmail.com>
2025-02-17 08:31:06 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyDTm9W1qwFNmWNJOt4bfbkTHIZHfLWnF5g",
