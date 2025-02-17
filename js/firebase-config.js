72061a4278d9768ef3aae3893c7e34132ac34daa
bielfg12 <gabriel.fernandesagil@gmail.com>
2025-02-17 08:28:35 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDTm9W1qwFNmWNJOt4bfbkTHIZHfLWnF5g",
+    authDomain: "site-25191.firebaseapp.com",
+    projectId: "site-25191",
+    storageBucket: "site-25191.firebasestorage.app",
+    messagingSenderId: "683821139505",
+    appId: "1:683821139505:web:2090a0ca432f6eec057710"
+  };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
